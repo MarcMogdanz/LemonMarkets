5e22a429fbbc553939da3118f0153e1af475d6f0
Marc Mogdanz <hey@marcmogdanz.de>
2022-12-23 01:03:02 +0100

    chore: extend linting rules

--- sdk/.eslintrc.js
+++ sdk/.eslintrc.js
@@ -3,5 +3,16 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "@typescript-eslint/no-explicit-any": "error",
+    // needs to be disabled for the typescript version of the rule to work
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    // needs to be disabled for the typescript version of the rule to work
+    "no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-expressions": "warn",
+  },
   root: true,
 };
